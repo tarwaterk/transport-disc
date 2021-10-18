161a10ed8bdd057bd3dd253e526abaaca83e6bef
Kevin Tarwater <tarwaterk@gmail.com>
2021-10-18 15:54:44 -0400

    Cache search results

--- src/components/cycleSearch/CycleSearch.tsx
+++ src/components/cycleSearch/CycleSearch.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Dispatch } from 'redux';
 import { useDispatch } from 'react-redux';
 import debounce from 'lodash/debounce';
 import { CYCLE_TYPES } from '../../reducers/cycleReducer';
@@ -11,16 +12,36 @@ const cycleQueryPayload = (query: string, data: Array<BikePoint>) => {
     };
 };
 
+const handleCycleDispatch = async (
+    e: React.ChangeEvent<HTMLInputElement>,
+    response: Response,
+    dispatch: Dispatch
+) => {
+    const data = await response.json();
+    dispatch({
+        type: CYCLE_TYPES.UPDATE_BIKE_POINTS,
+        payload: cycleQueryPayload(e.target.value, data)
+    });
+}
+
 const CycleSearch: React.FC = () => {
     const dispatch = useDispatch();
     const handleChange = debounce((e: React.ChangeEvent<HTMLInputElement>) => {
-        fetch(`https://api.tfl.gov.uk/BikePoint/Search?query=${e.target.value}`)
-        .then(async (response) => {
-            const data = await response.json();
-            dispatch({
-                type: CYCLE_TYPES.UPDATE_BIKE_POINTS,
-                payload: cycleQueryPayload(e.target.value, data)
-            });
+        const requestURL = `https://api.tfl.gov.uk/BikePoint/Search?query=${e.target.value}`;
+
+        caches.open('cycleCache')
+        .then(async (cache) => {
+            const cachedResp = await cache.match(requestURL);
+            if (cachedResp) {
+                handleCycleDispatch(e, cachedResp, dispatch);
+            } else {
+                fetch(`https://api.tfl.gov.uk/BikePoint/Search?query=${e.target.value}`)
+                .then(async (response) => {
+                    const respClone = response.clone();
+                    await cache.put(requestURL, respClone);
+                    handleCycleDispatch(e, response, dispatch);
+                });
+            }
         })
         .catch((err) => {
             dispatch({
