7abd81c166294d3abc98f0ae93c601b2c1da82e4
Kevin Tarwater <tarwaterk@gmail.com>
2021-10-18 13:45:54 -0400

    Debounce cycle hire search call

--- src/components/cycleSearch/CycleSearch.tsx
+++ src/components/cycleSearch/CycleSearch.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import debounce from 'lodash/debounce';
 import { CYCLE_TYPES } from '../../reducers/cycleReducer';
 
 const CycleSearch: React.FC = () => {
     const dispatch = useDispatch();
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const handleChange = debounce((e: React.ChangeEvent<HTMLInputElement>) => {
         fetch(`https://api.tfl.gov.uk/BikePoint/Search?query=${e.target.value}`)
         .then(async (response) => {
             const data = response.json();
@@ -12,8 +13,8 @@ const CycleSearch: React.FC = () => {
                 type: CYCLE_TYPES.UPDATE_BIKE_POINTS,
                 payload: data
             })
-        });
-    }
+        })}, 1000);
+
     return (
         <form>
             <input type='text' onChange={handleChange} />
